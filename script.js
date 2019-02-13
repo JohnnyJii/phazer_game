2255cdab341ca308c4fcc42fe09c30b852592bca
JohnnyJii <jukka.juntunen@metropolia.fi>
2019-02-13 22:45:23 +0200

    update

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
 let config = {
-    type:             Phaser.AUTO,
-    parent:           'phaser',
-    width:             800,
-    height:           600,
-    pixelArt:         true,
-    zoom:             2,
+    type: Phaser.AUTO,
+    parent: 'phaser',
+    width: 800,
+    height: 600,
+    pixelArt: true,
+    zoom: 2,
     physics: {
         default: 'arcade',
         arcade: {
@@ -16,7 +15,8 @@ let config = {
     scene: {
         preload: preload,
         create: create,
-        update: update
+        update: update,
+        render: render
     }
 };
 
@@ -38,30 +38,88 @@ game.CONFIG = {
     centerY: Math.round(0.5 * config.height)
 };
 
-function preload () {
+function preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 }
+    this.load.spritesheet('dude', 'assets/dude.png', {frameWidth: 32, frameHeight: 48}
     );
+    //skaalaus koko näytölle
+    game.scale.scalemode = Phaser.ScaleManager.SHOW_ALL;
+    game.scle.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+
+    game.load
 }
 
-function create () {
+function create() {
+    //koko näyttö mobiileille
+    if (!game.device.desktop) {
+        game.input.onDown.add(gofull, this);
+    }
     this.add.image(400, 300, 'sky');
 
     platforms = this.physics.add.staticGroup();
-
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
     player = this.physics.add.sprite(100, 450, 'dude');
-
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
+    //Mobiili kontrollit
+    // hyppynappi
+    jumpButton = game.aa.button(600, 500, 'jumpButton', null, this, 0, 1, 0, 1);
+    jumpButton.fixedToCamera = true;
+    jumpButton.events.onInputOver.add(function () {
+        jump = true;
+    });
+    jumpButton.events.onInputOut.add(function () {
+        jump = false;
+    });
+    jumpButton.events.onInputDown.add(function () {
+        jump = true;
+    });
+    jumpButton.events.onInputUp.add(function () {
+        jump = false;
+    });
+
+    //vasen nappi
+    leftButton = game.add.button(0, 472, 'buttonhorizontal', null, this, 0, 1, 0, 1);
+    leftButton.fixedToCamera = true;
+    leftButton.events.onInputOver.add(function () {
+        left = true;
+    });
+    leftButton.events.onInputOut.add(function () {
+        left = false;
+    });
+    leftButton.events.onInputDown.add(function () {
+        left = true;
+    });
+    leftButton.events.onInputUp.add(function () {
+        left = false;
+    });
+
+    //oikea nappi
+    rightButton = game.add.button(160, 472, 'buttonhorizontal', null, this, 0, 1, 0, 1);
+    rightButton.fixedToCamera = true;
+    rightButton.events.onInputOver.add(function () {
+        right = true;
+    });
+    rightButton.events.onInputOut.add(function () {
+        right = false;
+    });
+    rightButton.events.onInputDown.add(function () {
+        right = true;
+    });
+    rightButton.events.onInputUp.add(function () {
+        right = false;
+    });
+
+
+    //Näpppäimistö kontrollit
     this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
@@ -70,17 +128,17 @@ function create () {
     });
     this.anims.create({
         key: 'turn',
-        frames: [ {key: 'dude', frame: 4}],
+        frames: [{key: 'dude', frame: 4}],
         frameRate: 20
     });
     this.anims.create({
         key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
+        frames: [{key: 'dude', frame: 4}],
         frameRate: 20
     });
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
         frameRate: 10,
         repeat: -1
     });
@@ -91,7 +149,7 @@ function create () {
     stars = this.physics.add.group({
         key: 'star',
         repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70}
+        setXY: {x: 12, y: 0, stepX: 70}
     });
     stars.children.iterate(function (child) {
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
@@ -104,10 +162,13 @@ function create () {
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
     this.physics.add.collider(bombs, platforms);
-
     this.physics.add.overlap(player, stars, collectStar, null, this);
     this.physics.add.collider(player, bombs, hitBomb, null, this);
 
+}
+
+function render() {
+    game.debug.text('jump:' + jump +  'left:' + left + ' right:' + right, 20, 20);
 }
 
 function resize() {
@@ -134,7 +195,7 @@ resize();
 
 //ohjaimet näppikselle//
 
-function update () {
+function update() {
     if (gameOver) {
         return;
     }
@@ -152,6 +213,30 @@ function update () {
         player.setVelocityY(-330);
     }
 }
+
+function mobile() {
+    if (left) {
+        player.scale.x = -1;
+        player.body.moveLeft(500);
+        player.animations.play('walk');
+    } else if (right) {
+        player.scale.x = 1;
+        player.body.moveRight(500);
+        player.animations.play('walk');
+    } else {
+        player.loadTexture('dude', 0);
+    }
+    if (jump) {
+        jump_now();
+        player.loadTexture('dude', 5);
+    }
+    if (game.input.currentPointers == 0 && !game.input.activePointer.isMouse) {
+        right = false;
+        left = false;
+        jump = false;
+    }
+};
+
 
 function collectStar(player, star) {
     star.disableBody(true, true);
